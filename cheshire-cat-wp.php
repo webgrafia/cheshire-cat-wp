29178ee72868ac9730dd40d02d27f5f63bb95891
marco <spappolo>
2023-04-13 17:07:39 +0200

    first commit

---
+++ cheshire-cat-wp.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Cheshire Cat WP
+Description: Plugin to integrate Cheshire Cat with WordPress
+Version: 0.1
+Author: Marco Buttarini
+Author URI: https://bititup.it/
+License: GPL2
+*/
+
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ .'/inc/admin.php';
+require_once __DIR__ .'/inc/shortcodes.php';
+require_once __DIR__ .'/inc/ajax.php';
+
